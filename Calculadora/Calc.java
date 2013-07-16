d7bb6efd56939c33e5b408d227167ba146d3971c
Xarlox <carfran1925@hotmail.com>
2013-07-16 16:49:55 -0500

    Calculadora
    Una calculadora basica con java

---
+++ Calculadora/Calc.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculadora;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Calc {
+    public static void main (String []args){
+        new Calculadora().show();
+    }
+    
+}
